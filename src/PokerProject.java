5fecf628e4e16086fa6b6cf9e04fc38b39913c86
afrazs2 <afrazs2@nycstudents.net>
2026-02-06 08:29:05 -0500

    Add files via upload

--- src/PokerProject.java
+++ src/PokerProject.java
@@ -19,63 +19,66 @@ public class PokerProject {
         while (Scan.hasNextLine()) {
 
             String line = Scan.nextLine().trim();
-            if (line.isEmpty()) continue;
 
-            int bar = line.indexOf('|');
-            if (bar != -1) line = line.substring(0, bar);
+            if (!line.isEmpty()) {
 
-            String[] cards = line.split(",");
+                int bar = line.indexOf('|');
+                if (bar != -1) {
+                    line = line.substring(0, bar);
+                }
 
-            String[] seen = new String[5];
-            int[] count = new int[5];
-            int used = 0;
+                String[] cards = line.split(",");
 
-            for (int i = 0; i < 5; i++) {
-                String card = cards[i].trim();
+                String[] seen = new String[5];
+                int[] count = new int[5];
+                int used = 0;
 
-                int pos = -1;
-                for (int j = 0; j < used; j++) {
-                    if (seen[j].equals(card)) {
-                        pos = j;
-                        break;
+                for (int i = 0; i < 5; i++) {
+                    String card = cards[i].trim();
+
+                    int pos = -1;
+                    for (int j = 0; j < used; j++) {
+                        if (seen[j].equals(card)) {
+                            pos = j;
+                            break;
+                        }
+                    }
+
+                    if (pos == -1) {
+                        seen[used] = card;
+                        count[used] = 1;
+                        used++;
+                    } else {
+                        count[pos]++;
                     }
                 }
 
-                if (pos == -1) {
-                    seen[used] = card;
-                    count[used] = 1;
-                    used++;
-                } else {
-                    count[pos]++;
+                int max = 0;
+                for (int c : count) {
+                    if (c > max) max = c;
                 }
-            }
 
-            int max = 0;
-
-            for (int c : count) {
-                if (c > max) max = c;
-            }
-
-            if (used == 1) {
-                five++;
-            } else if (used == 2) {
-                if (max == 4) four++;
-                else full++;
-            } else if (used == 3) {
-                if (max == 3) three++;
-                else two++;
-            } else if (used == 4) {
-                one++;
-            } else {
-                high++;
+                if (used == 1) {
+                    five++;
+                } else if (used == 2) {
+                    if (max == 4) four++;
+                    else full++;
+                } else if (used == 3) {
+                    if (max == 3) three++;
+                    else two++;
+                } else if (used == 4) {
+                    one++;
+                } else {
+                    high++;
+                }
             }
         }
 
-        System.out.println("Number of five of a kind hands: " + five);
-        System.out.println("Number of full house hands: " + full);
-        System.out.println("Number of four of a kind hands: " + four);
+        System.out.println(" Number of five of a kind hands: " + five);
+        System.out.println(" Number of full house hands: " + full);
+        System.out.println(" Number of four of a kind hands: " + four);
         System.out.println("Number of three of a kind hands: " + three);
-        System.out.println("Number of two pair hands: " + two);
+        System.out.println(" Number of two pair hands: " + two);
         System.out.println("Number of one pair hands: " + one);
         System.out.println("Number of high card hands: " + high);
     }
